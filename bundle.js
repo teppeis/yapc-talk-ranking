4c8dab01ecc02a978fa44bb28a7e2940f6c73cc0
teppeis <teppeis@gmail.com>
2015-06-20 03:00:48 +0900

    update ranking

--- bundle.js
+++ bundle.js
@@ -27,7 +27,7 @@
 
 },{}],2:[function(require,module,exports){
 module.exports={
-  "date": 1434733247759,
+  "date": 1434736846865,
   "ranking": [
     {
       "path": "/2015/talk/show/de9e7a1e-136d-11e5-a9fc-d9f87d574c3a",
