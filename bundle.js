905ac0974941d5658b562329f9fc15379226759c
teppeis <teppeis@gmail.com>
2015-06-21 07:00:51 +0900

    update ranking

--- bundle.js
+++ bundle.js
@@ -27,7 +27,7 @@
 
 },{}],2:[function(require,module,exports){
 module.exports={
-  "date": 1434834048232,
+  "date": 1434837650549,
   "ranking": [
     {
       "path": "/2015/talk/show/de9e7a1e-136d-11e5-a9fc-d9f87d574c3a",
